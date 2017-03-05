44f0b12b3533cfa7125d563d055cb3953e6f7622
imranziad <imranziad@gmail.com>
2017-03-06 01:55:13 +0600

    remove_whitespace function added

--- Main.cpp
+++ Main.cpp
@@ -2,15 +2,35 @@
 #include <cstdio>
 using namespace std;
 
-int main()
-{
-    freopen("input.txt","r",stdin);
+string str;
+
+string remove_whitespace(string s) {
+    /*
+        Takes a string and
+        returns it after removing
+        all space characters.
+    */
+    string ret = "";
+    int len = s.size();
+
+    for(int i = 0; i < len; i++) {
+        if(s[i] != ' ')
+            ret += s[i];
+    }
+    return ret;
+}
+
+int main() {
+    //freopen("input.txt","r",stdin);
     //freopen("output.txt","w",stdout);
     string s;
 
-    cin >> s;
+    getline(cin, s);
 
-    cout << s << endl;
+    str = remove_whitespace(s);
+
+    cout << "input: " << s << endl;
+    cout << "whitespace removed: " << str << endl;
 
     return 0;
 }
