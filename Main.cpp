a8e439c757079e61dac818529da10e4d6d6fbe7b
imranziad <imranziad@gmail.com>
2017-03-06 02:14:32 +0600

    infix_to_postfix() function added

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 string str;
+char look_ahead;
+int look_ahead_idx;
 
 string remove_whitespace(string s) {
     /*
@@ -20,6 +22,53 @@ string remove_whitespace(string s) {
     return ret;
 }
 
+void match(char ch) {
+    look_ahead_idx++;
+    if(look_ahead_idx < str.size())
+        look_ahead = str[ look_ahead_idx ];
+    else
+        look_ahead = '\0';
+}
+
+void term() {
+    if(look_ahead >= '0' && look_ahead <= '9') {
+        cout << look_ahead;
+        match(look_ahead);
+    }
+    else {
+        cout << "\nSyntax Error!" << endl;
+    }
+}
+
+void rest() {
+    if(look_ahead == '+') {
+        match('+');
+        term();
+        cout << "+";
+        rest();
+    }
+    else if(look_ahead == '-') {
+        match('-');
+        term();
+        cout << "-";
+        rest();
+    }
+    else {  }
+}
+
+void expr() {
+    term();
+    rest();
+}
+
+void infix_to_postfix() {
+    look_ahead_idx = 0;
+    look_ahead = str[0];
+    cout << "Postfix: ";
+    expr();
+    cout << endl;
+}
+
 int main() {
     //freopen("input.txt","r",stdin);
     //freopen("output.txt","w",stdout);
@@ -31,6 +80,8 @@ int main() {
 
     cout << "input: " << s << endl;
     cout << "whitespace removed: " << str << endl;
+
+    infix_to_postfix();
 
     return 0;
 }
