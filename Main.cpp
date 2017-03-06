d3444d21a52587df8499986df914908303102011
imranziad <imranziad@gmail.com>
2017-03-06 06:06:23 +0600

    separated postfix operation from the Main driver source

--- Main.cpp
+++ Main.cpp
@@ -2,11 +2,17 @@
 #include <cstdio>
 using namespace std;
 
+/**
+    custom headers are included
+    after namespace to avoid repetition
+    in other files.
+*/
+
+#include "postfix.h"
+
 string str;
 char look_ahead;
 int look_ahead_idx, level;
-
-void expr();
 
 string remove_whitespace(string s) {
     /*
@@ -24,89 +30,8 @@ string remove_whitespace(string s) {
     return ret;
 }
 
-void match(char ch) {
-    if(ch != look_ahead) return ;
-    look_ahead_idx++;
-    if(look_ahead_idx < str.size())
-        look_ahead = str[ look_ahead_idx ];
-    else
-        look_ahead = '\0';
-}
-
-void factor() {
-    if(look_ahead >= '0' && look_ahead <= '9') {
-        cout << look_ahead;
-        match(look_ahead);
-    }
-    else if(look_ahead == '(') {
-        match(look_ahead);
-        int pre_level = level;
-        level++;
-        expr();
-        if(look_ahead == ')' && level-1 == pre_level) level--;
-        else cout << "\nSyntax Error!" << endl;
-        match(look_ahead);
-    }
-    else {
-        cout << "\nSyntax Error!" << endl;
-    }
-}
-
-void factor_rest() {
-    while(true) {
-        if(look_ahead == '*') {
-            match('*');
-            factor();
-            cout << "*";
-        }
-        else if(look_ahead == '/') {
-            match('/');
-            factor();
-            cout << "/";
-        }
-        else break;
-    }
-}
-
-void term() {
-    factor();
-    factor_rest();
-}
-
-void rest() {
-    while(true) {
-        if(look_ahead == '+') {
-            match('+');
-            term();
-            cout << "+";
-        }
-        else if(look_ahead == '-') {
-            match('-');
-            term();
-            cout << "-";
-        }
-        else break;
-    }
-}
-
-void expr() {
-    term();
-    rest();
-}
-
-void infix_to_postfix() {
-    level = 0;
-    look_ahead_idx = 0;
-    look_ahead = str[0];
-    cout << "Postfix: ";
-    expr();
-    if(look_ahead_idx < str.size())
-        cout << "\nSyntax Error!" << endl;
-    cout << endl;
-}
-
 int main() {
-    //freopen("input.txt","r",stdin);
+    freopen("input.txt","r",stdin);
     //freopen("output.txt","w",stdout);
     string s;
 
@@ -117,7 +42,7 @@ int main() {
     cout << "input: " << s << endl;
     cout << "whitespace removed: " << str << endl;
 
-    infix_to_postfix();
+    Postfix pf(str);
 
     return 0;
 }
---
+++ postfix.h
@@ -0,0 +1,97 @@
+
+/**
+    Contains Postfix class,
+    that takes a string in constructor
+    and performs the infix to postfix
+    translation if the string is a valid
+    infix expression.
+**/
+
+class Postfix {
+    string str;
+    char look_ahead;
+    int look_ahead_idx, level;
+
+public:
+    Postfix(string s) {
+        str = s;
+        level = 0;
+        look_ahead_idx = 0;
+        look_ahead = str[0];
+        cout << "Postfix: ";
+        expr();
+        if(look_ahead_idx < str.size())
+            cout << "\nSyntax Error!" << endl;
+        cout << endl;
+    }
+
+    void match(char ch) {
+        if(ch != look_ahead) return ;
+        look_ahead_idx++;
+        if(look_ahead_idx < str.size())
+            look_ahead = str[ look_ahead_idx ];
+        else
+            look_ahead = '\0';
+    }
+
+    void factor() {
+        if(look_ahead >= '0' && look_ahead <= '9') {
+            cout << look_ahead;
+            match(look_ahead);
+        }
+        else if(look_ahead == '(') {
+            match(look_ahead);
+            int pre_level = level;
+            level++;
+            expr();
+            if(look_ahead == ')' && level-1 == pre_level) level--;
+            else cout << "\nSyntax Error!" << endl;
+            match(look_ahead);
+        }
+        else {
+            cout << "\nSyntax Error!" << endl;
+        }
+    }
+
+    void factor_rest() {
+        while(true) {
+            if(look_ahead == '*') {
+                match('*');
+                factor();
+                cout << "*";
+            }
+            else if(look_ahead == '/') {
+                match('/');
+                factor();
+                cout << "/";
+            }
+            else break;
+        }
+    }
+
+    void term() {
+        factor();
+        factor_rest();
+    }
+
+    void rest() {
+        while(true) {
+            if(look_ahead == '+') {
+                match('+');
+                term();
+                cout << "+";
+            }
+            else if(look_ahead == '-') {
+                match('-');
+                term();
+                cout << "-";
+            }
+            else break;
+        }
+    }
+
+    void expr() {
+        term();
+        rest();
+    }
+};
